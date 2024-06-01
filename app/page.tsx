2e91fe96db309ca46ed2cf0ba2ac34746236bc2d
Brandon Quon <bqa7@sfu.ca>
2024-05-31 21:44:59 -0700

    Filled out the ABOUT US page and need to work on TEAMS page

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,39 @@ export default function Home() {
 		{
 			title: 'Overwatch',
 			picture: '/overwatch.jpeg'
-		}
+		},
+        {
+            title: 'Fighting Games',
+            picture: '/fighting.jpg'
+        },
+        {
+            title: 'Trading Card Games',
+            picture: '/pokemon.jpg'
+        },
+        {
+            title: 'Destiny 2',
+            picture: '/destiny.jpg'
+        },
+        {
+            title: 'Rainbow 6 Siege',
+            picture: '/r6.jpg'
+        },
+        {
+            title: 'FIFA',
+            picture: '/fifa.jpg'
+        },
+        {
+            title: 'Call of Duty',
+            picture: '/cod.jpg'
+        },
+        {
+            title: 'Smite',
+            picture: '/smite.jpg'
+        },
+        {
+            title: 'Pokemon',
+            picture: '/pokemon1.jpg'
+        }
     ];
 
     const count = useMotionValue(1000);
@@ -56,7 +88,8 @@ export default function Home() {
         <main className="relative">
 
             {/* HOME PAGE */}
-            <div className="h-screen w-screen bg-black flex flex-col justify-center items-center relative text-white text-center">
+            <div className="h-screen w-screen flex flex-col justify-center items-center relative text-white text-center">
+                <Image src='/homebg.png' alt="homebg" width={0} height={0} className="absolute top-0 left-0 -z-10 scale-125" sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
                 <p className="text-5xl font-normal mb-8">
                     JOIN SFU'S LARGEST GAMING CLUB <br />
                     WITH OVER <motion.span className="font-bold text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">{rounded}</motion.span> MEMBERS
@@ -64,152 +97,90 @@ export default function Home() {
                 <motion.button whileTap={{ scale: 0.95 }} whileHover={{ scale: 1.05 }} id="btn-grad" className="text-3xl font-bold italic">GAME WITH US</motion.button>
             </div>
 
-<<<<<<< HEAD
             {/* ABOUT US PAGE */}
-            <div className="h-screen w-screen text-6xl flex flex-col justify-center items-center bg-[silver]">
-                <div className="w-1/2 mb-10 mr-[27%] text-black">
+            <div className="h-screen w-screen text-6xl flex flex-col justify-center items-center bg-[#d3d3d3]">
+                <div className="w-[75vw] mb-10 text-black">
                     <h1 className="text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">ABOUT US.</h1>
-                    <p className="text-xl w-1/2">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium natus sapiente commodi.
-                        Aliquid sunt tempore iste repellendus explicabo dignissimos placeat.
+                    <p className="text-xl font-semibold w-2/5">
+                        The SFU Gaming and Esports Club is a student-run nonprofit organization at Simon Fraser University that fosters a vibrant gaming community for both competitive and casual players. 
+                        Join us for exciting tournaments, in-houses, in-person events, and more!
                     </p>
                 </div>
 
-                <div className="w-screen h-[45vh] flex justify-center gap-6 relative">
-                    <div className="w-[25%] relative">
-                        <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg h-full flex flex-col">
-                            <img
-                                alt=""
-                                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                                className="w-full h-[60%] object-cover"
-                            />
-
-                            <div className="bg-white p-4 sm:p-6 h-[40%] overflow-auto">
-                                <a href="#">
-                                    <h3 className="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
-                                </a>
-
-                                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-                                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-                                    dignissimos. Molestias explicabo corporis voluptatem?
-                                </p>
+                <div className="w-[75vw] h-[50vh] flex justify-center gap-6 relative">
+                    <div className="w-1/3 relative">
+                        <article className="overflow-hidden rounded-lg shadow-lg h-full flex flex-col">
+                            <div className="relative w-full h-[65%]">
+                                <Image src='/ex1.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
                             </div>
-                        </article>
-=======
-			
-			{/* Div of the whole page section */}
-            <div className="w-screen relative">
 
-				<div className="h-screen w-screen text-6xl flex flex-col justify-center items-center bg-[silver] sticky top-0">
-                    <div className="w-1/2 mb-10 mr-[27%] text-black">
-                        <h1 className="text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">ABOUT US.</h1>
-                        <p className="text-xl w-1/2">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium natus sapiente commodi.
-                            Aliquid sunt tempore iste repellendus explicabo dignissimos placeat.
-                        </p>
->>>>>>> c57f8ddc3449f4605c5facf258c180987e23adc8
-                    </div>
+                            <div className="bg-white p-2 sm:p-6 h-[35%] overflow-auto">
 
-                    <div className="w-[25%]">
-                        <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg h-full flex flex-col">
-                            <img
-                                alt=""
-                                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                                className="w-full h-[60%] object-cover"
-                            />
+                                <h3 className="text-xl text-black">Game in the Gamer's Lounge!</h3>
 
-                            <div className="bg-white p-4 sm:p-6 h-[40%] overflow-auto">
-                                <a href="#">
-                                    <h3 className="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
-                                </a>
 
-                                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-                                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-                                    dignissimos. Molestias explicabo corporis voluptatem?
+                                <p className="mt-2 text-base text-gray-500">
+                                Drop into the Gamer's Lounge on campus to play on one of our 16 high-performance PCs or enjoy console gaming on a big-screen TV.
                                 </p>
                             </div>
                         </article>
                     </div>
 
-                    <div className="w-[25%]">
-                        <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg h-full flex flex-col">
-                            <img
-                                alt=""
-                                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-                                className="w-full h-[60%] object-cover"
-                            />
+                    <div className="w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg h-full flex flex-col">
+                            <div className="relative w-full h-[65%]">
+                                <Image src='/ex2.jpg' alt="ex2" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+                            </div>
 
-                            <div className="bg-white p-4 sm:p-6 h-[40%] overflow-auto">
-                                <a href="#">
-                                    <h3 className="mt-0.5 text-lg text-gray-900">How to position your furniture for positivity</h3>
-                                </a>
+                            <div className="bg-white p-2 sm:p-6 h-[35%] overflow-auto">
 
-                                <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-                                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
-                                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
-                                    dignissimos. Molestias explicabo corporis voluptatem?
+                                <h3 className="text-xl text-black">Connect with Like-Minded Gamers!</h3>
+
+
+                                <p className="mt-2 text-base text-gray-500">
+                                    Forge friendships and bond over shared interests at our gatherings. 
+                                    Join us for lively conversations, and the chance to meet others who share your passion for gaming.
+                                </p>
+                            </div>
+                        </article>
+                    </div>
+
+                    <div className="w-1/3">
+                        <article className="overflow-hidden rounded-lg shadow-lg h-full flex flex-col">
+                            <div className="relative w-full h-[65%]">
+                                <Image src='/ex3.jpg' alt="ex3" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+                            </div>
+
+                            <div className="bg-white p-2 sm:p-6 h-[35%] overflow-auto">
+
+                                <h3 className="text-xl text-black">Engage in Diverse Student-Run Events!</h3>
+
+                                <p className="mt-2 text-base text-gray-500">
+                                    Participate in a variety of student-run events, ranging from competitive tournaments and 
+                                    casual game nights to community meetups and themed gaming sessions.
                                 </p>
                             </div>
                         </article>
                     </div>
                 </div>
-
-                <motion.div
-                    style={{
-                        zIndex: mousePosition > 0 ? '10' : '-10',
-                        visibility: mousePosition > 0 ? 'visible' : 'hidden'
-                    }}
-                    animate={{
-                        WebkitMaskSize: `${mousePosition * 3.5}px`,
-                    }}
-                    transition={{ type: "spring", ease: "easeOut", stiffness: 0, damping: 0 }}
-                    id="mask" className="h-screen w-screen text-6xl flex flex-col justify-center items-center relative bg-[#131313]">
-                    <Image className="absolute top-[30vh] left-1/2 -translate-x-1/2" src='/racc.png' height={120} width={120} alt="racc" />
-                    <div className="w-1/2 mb-[40px] mr-[27%] text-white">
-                        <h1 className="text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">GAMING TEAMS.</h1>
-                        <p className="text-xl w-1/2">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium natus sapiente commodi.
-                            Aliquid sunt tempore iste repellendus explicabo dignissimos placeat.
-                        </p>
-                    </div>
-                    <div className="w-[80vw] h-[45vh] relative bg-slate-200 flex gap-3">
-                        {games.map((game, index) => (
-                            <div key={index} id="game-card" className="relative w-[12vw] duration-300 cursor-pointer hover:w-[20vw]">
-                                <Image id="game-picture" className="w-full h-full object-cover brightness-50 hover:brightness-100" src={game.picture} alt={game.title} layout="fill" />
-                                <h2 className="absolute bottom-0 left-0 h-full bg-black bg-opacity-50 text-white text-center px-2 font-semibold" style={{ writingMode: 'vertical-rl', transform: 'rotate(180deg)' }}>{game.title}</h2>
-                            </div>
-                        ))}
-                    </div>
-
-
-                </motion.div>
-
             </div>
 
-<<<<<<< HEAD
             {/* GRID CONTAINER */}
-            <div className="w-screen h-screen bg-black relative flex justify-center items-end">
-                <div className="w-[80%] h-[80%] grid grid-cols-3 grid-rows-4 gap-4">
+            <div className="w-screen h-screen bg-[#131313] relative flex flex-col justify-center items-center">
+                <h1 className="w-[75%] text-7xl font-bold mb-10 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">TEAMS.</h1>
+                <div className="w-[75%] h-[80%] grid grid-cols-3 grid-rows-4 gap-12">
                     {games.map((game, index) => (
-                        <div key={index} className="flex flex-col w-full h-full relative">
-                            <div className="w-full h-full bg-blue-30 relative overflow-hidden">
-                                <Image src={game.picture} alt="GamePicture" className="brightness-50 duration-300 hover:brightness-100 hover:scale-110" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+                        <div key={index} id="game-cell" className="flex flex-col w-full h-full relative rounded-2xl overflow-hidden">
+                            <div className="w-full h-full bg-blue-30 relative">
+                                <Image src={game.picture} id="game-picture" alt="GamePicture" className="duration-300 brightness-50" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
                             </div>
                         
-                            <p className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center mt-2 text-xl">{game.title}</p>
+                            <p className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 text-center text-4xl font-bold">{game.title}</p>
                         </div>
                     ))}
                 </div>
             </div>
 
-=======
-            <div className="h-screen w-screen bg-blue-500 flex justify-center items-center relative"></div>
->>>>>>> c57f8ddc3449f4605c5facf258c180987e23adc8
         </main>
     );
 }
