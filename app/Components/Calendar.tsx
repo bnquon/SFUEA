9358d83e3f5f9c3c64740d8777054d9ed232664f
Brandon Quon <bqa7@sfu.ca>
2024-06-01 13:32:33 -0700

    Worked on bare bones event scheduling, need to make events a type though

--- app/Components/Calendar.tsx
+++ app/Components/Calendar.tsx
@@ -6,16 +6,34 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 const localizer = momentLocalizer(moment)
 
 export const BasicCalendar = () => {
+
+  type Events = {
+    title: string;
+    picture: string;
+}
+
+  const events = [
+    {
+      title: 'League In-house',
+      start: new Date(),
+      end: new Date(new Date().setHours(new Date().getHours() + 1)),
+    }
+  ]
+
   return (
-    <div className='h-screen w-screen flex justify-center items-center relative'>
-        <div className='w-4/5 h-4/5'>
-            <Calendar
-            localizer={localizer}
-            startAccessor="start"
-            endAccessor="end"
-            length={1}
-            />
-        </div>
+    <div className='h-screen w-screen flex flex-col gap-10 justify-center items-center relative'>
+
+      <h1 className="w-4/5 text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">EVENT SCHEDULE.</h1>
+
+      <div className='w-4/5 h-[75%]'>
+          <Calendar
+          events={events}
+          localizer={localizer}
+          startAccessor="start"
+          endAccessor="end"
+          length={1}
+          />
+      </div>
     </div>
   )
 }
