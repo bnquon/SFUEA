fb6b435deaeabbaac2ae48c3b0a4125012f7d7c0
Brandon Quon <bqa7@sfu.ca>
2024-06-01 19:25:13 -0700

    <Feature> Styled the calendar a bit

--- app/Components/Calendar.tsx
+++ app/Components/Calendar.tsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import { Calendar, momentLocalizer } from "react-big-calendar"
-import moment from "moment"
+import React from 'react';
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
 export const BasicCalendar = () => {
 
   type Events = {
     title: string;
     picture: string;
-}
+  }
 
   const events = [
     {
       title: 'League In-house',
-      start: new Date(),
-      end: new Date(new Date().setHours(new Date().getHours() + 1)),
+      start: moment("2024-06-01T11:00:00").toDate(),
+      end: moment("2024-06-01T12:00:00").toDate(),
     }
-  ]
+  ];
 
   return (
-    <div className='h-screen w-screen flex flex-col gap-10 justify-center items-center relative'>
+    <div className='h-[100vh] w-screen flex flex-col gap-10 justify-center items-center relative bg-[silver]'>
+      <h1 className="w-3/4 text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">
+        EVENT SCHEDULE.
+      </h1>
 
-      <h1 className="w-4/5 text-7xl font-bold mb-6 text-transparent bg-clip-text bg-gradient-to-r from-red-700 to-red-400">EVENT SCHEDULE.</h1>
-
-      <div className='w-4/5 h-[75%]'>
-          <Calendar
+      <div className='w-3/4 h-4/5 relative'>
+        <Calendar
           events={events}
           localizer={localizer}
           startAccessor="start"
           endAccessor="end"
-          length={1}
-          />
+          style={{ height: '100%' }}
+        />
       </div>
     </div>
-  )
+  );
 }
