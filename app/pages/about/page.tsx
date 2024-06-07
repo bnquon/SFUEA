f34104119971962bc0ce812af6c6d6289c343547
Brandon Quon <bqa7@sfu.ca>
2024-06-06 18:47:58 -0700

    Need to work on other pages

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,18 +1,12 @@
 import Image from "next/image";
 
-
-
 export default function Home() {
     return (
-    	<main className="flex flex-col max-w-full overflow-x-hidden">
-      		<div id="section1" className="h-screen w-screen bg-blue-900 flex justify-center items-center">
-        		About page
-      		</div>
-            <div id="section2" className="h-screen w-screen bg-indigo-900">
-        		
-      		</div>
-      		<div id="section3" className="h-screen w-screen bg-violet-900">
-        		
+    	<main className="flex flex-col max-w-full overflow-x-hidden pt-[15vh]">
+      		<div id="section1" className="w-screen relative bg-white flex justify-center items-center">
+        		<div className="w-[75vw] rounded-lg">
+					<p className="text-red-500 text-7xl">About the SFU Gaming and Esports Club</p>
+				</div>
       		</div>
     	</main>
   	);
