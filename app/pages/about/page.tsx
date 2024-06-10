f87a8ea23554b4271a6dbee0b449e00673638027
Brandon Quon <bqa7@sfu.ca>
2024-06-09 21:57:23 -0700

    <Feature> Added all the blob headers for the pages except the gaming teams

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
       		<div id="section1" className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[15vh] pb-[10vh]">
         		<div className="w-[75vw] items-center flex justify-evenly text-black py-10 px-10 rounded-xl overflow-hidden relative z-0">
-					<Image className="absolute brightness-50 z-[1]" src='/BWPattern.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+					<Image className="absolute brightness-[35%] z-[1]" src='/blob2.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 					<div className="flex flex-col w-3/5 gap-6 text-white z-[2]">
 						<p className="font-bold text-3xl text-red-500">Who We Are</p>
 						<p className="text-5xl font-extrabold">SFU GAMING AND ESPORTS CLUB IS THE PREMIUM LIFESTYLE BRAND FOR THE GAMING GENERATION.</p>
@@ -14,7 +14,8 @@ export default function Home() {
 					<Image className="z-[2]" src='/real-logo.png' alt="ex1" width={250} height={250}></Image>
 				</div>
       		</div>
-				
+
+			<div className="w-screen h-[75vh] bg-[#d3d3d3]">sdfsdf</div>
 
     	</main>
   	);
--- app/pages/gamingLounge/page.tsx
+++ app/pages/gamingLounge/page.tsx
@@ -4,16 +4,20 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-    	<main className="flex flex-col max-w-full overflow-x-hidden">
-      		<div id="section1" className="h-screen w-screen bg-orange-900 flex justify-center items-center">
-        		Events page
+    	<main className="relative">
+
+      		<div id="section1" className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[15vh] pb-[10vh]">
+        		<div className="w-[75vw] items-center flex justify-evenly text-black py-10 px-10 rounded-xl overflow-hidden relative z-0">
+					<Image className="absolute brightness-50 z-[1]" src='/blob1.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+					<div className="flex flex-col w-3/5 gap-6 text-white z-[2]">
+						<p className="font-bold text-3xl text-red-500">Gamers' Lounge</p>
+						<p className="text-5xl font-extrabold">SFU GAMING AND ESPORTS CLUB IS THE PREMIUM LIFESTYLE BRAND FOR THE GAMING GENERATION.</p>
+					</div>
+					<Image className="z-[2]" src='/real-logo.png' alt="ex1" width={250} height={250}></Image>
+				</div>
       		</div>
-            <div id="section2" className="h-screen w-screen bg-amber-900">
-        		
-      		</div>
-      		<div id="section3" className="h-screen w-screen bg-yellow-900">
-        		
-      		</div>
+				
+
     	</main>
   	);
 }
