2004367243f49548edbab9ea1d732e3f1a3fff79
Brandon Quon <bqa7@sfu.ca>
2024-06-16 14:45:35 -0700

    IDK

--- app/pages/gamingLounge/page.tsx
+++ app/pages/gamingLounge/page.tsx
@@ -19,39 +19,56 @@ export default function Home() {
       		</div>
 
 			<div className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[5vh] pb-[5vh]">
-				<div className="w-[75vw] flex box-border justify-between relative text-black h-[50vh] gap-2">
-					<div className="w-1/2 h-full relative flex flex-col bg-white rounded-xl p-6 overflow-hidden">
-						<Image className="absolute top-0 left-0" src="https://images.unsplash.com/photo-1593640408182-31c70c8268f5?q=80&w=2642&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="ex2" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
-						<div className="isolate w-1/2 h-full rounded-xl backdrop-blur-md bg-white/40 shadow-lg ring-1 ring-black/5">
-
+				<div className="w-[75vw] py-8 text-black">
+					<p className="text-red-500 text-xl">Icon here</p>
+					<p className="text-5xl mb-[5vh]">Tech Specs</p>
+					<div className="w-full flex gap-6 box-border">
+						<div className="w-1/2 h-[25vh]">
+							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+						</div>
+						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
 						</div>
 					</div>
+				</div>
 
-					<div className="w-1/2 h-full flex flex-col box-border gap-2">
-						<div className="w-full h-1/2 relative flex flex-col box-border">
-							<div className="w-full h-full relative flex flex-col bg-white rounded-xl p-6 overflow-hidden">
-								<Image className="absolute top-0 left-0" src="https://images.unsplash.com/photo-1593640408182-31c70c8268f5?q=80&w=2642&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="ex2" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
-								<div className="isolate w-1/2 h-full rounded-xl backdrop-blur-md bg-white/40 shadow-lg ring-1 ring-black/5">
-
-								</div>
-							</div>
+				<div className="w-[75vw] py-8 border-t-2 border-b-2 border-black text-black">
+					<p className="text-red-500 text-xl">Icon here</p>
+					<p className="text-5xl mb-[5vh]">Tech Specs</p>
+					<div className="w-full flex gap-6 box-border">
+						<div className="w-1/2 h-[25vh]">
+							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 						</div>
-						<div className="w-full h-1/2 relative flex flex-col">
-							<div className="w-full h-full relative flex flex-col bg-white rounded-xl p-6 overflow-hidden">
-								<Image className="absolute top-0 left-0" src="https://images.unsplash.com/photo-1593640408182-31c70c8268f5?q=80&w=2642&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="ex2" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
-								<div className="isolate w-1/2 h-full rounded-xl backdrop-blur-md bg-white/40 shadow-lg ring-1 ring-black/5">
-						
-								</div>
-							</div>
+						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
 						</div>
 					</div>
-					
+				</div>
+
+				<div className="w-[75vw] py-8 text-black">
+					<p className="text-red-500 text-xl">Icon here</p>
+					<p className="text-5xl mb-[5vh]">Tech Specs</p>
+					<div className="w-full flex gap-6 box-border">
+						<div className="w-1/2 h-[25vh]">
+							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+						</div>
+						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+						</div>
+					</div>
 				</div>
 			</div>
 
 			<div className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[5vh] pb-[10vh]">
-				<div className="w-[75vw] h-[35vh] rounded-xl bg-red-500">
-
+				<div className="w-[75vw] h-[35vh] rounded-xl bg-red-500 flex flex-col justify-center items-center p-6">
+					<p className="text-5xl font-bold">Title</p>
+					<p className="text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
 				</div>
 			</div>
 				
