72824c75c937743b67d3ba990c110bb5f85c89d9
Brandon Quon <bqa7@sfu.ca>
2024-06-15 22:50:57 -0700

    Working on gaming lounge page

--- app/pages/gamingLounge/page.tsx
+++ app/pages/gamingLounge/page.tsx
@@ -16,6 +16,58 @@ export default function Home() {
 					<Image className="z-[2]" src='/real-logo.png' alt="ex1" width={250} height={250}></Image>
 				</div>
       		</div>
+
+			<div className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[5vh] pb-[10vh]">
+				<div className="w-[75vw] flex box-border justify-between relative text-black h-[75vh]">
+					<div className="w-2/5 flex flex-col relative h-full gap-2 box-border">
+						<div className="w-full h-1/3 flex-grow flex flex-col py-6">
+							<h2 className="text-3xl font-bold">ICON HERE</h2>
+							<h2 className="text-3xl font-bold">Our Mission</h2>
+							<p className="text-xl">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							</p>
+						</div>
+						<Image
+							className="rounded-xl flex-grow"
+							src="https://images.unsplash.com/photo-1716249738093-623a70ad6531?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+							alt="ex1"
+							width={0}
+							height={0}
+							sizes="100vw"
+							style={{ width: '100%', height: '66.667%', objectFit: 'cover' }}
+						/>
+					</div>
+					<div className="w-1/2 h-full relative flex flex-col box-border gap-2">
+						<div className="w-full h-1/3 flex-grow flex flex-col p-6 bg-white rounded-xl relative">
+							<h2 className="h-1/4 text-3xl font-bold">ICON HERE</h2>
+							<h2 className="h-1/4 text-3xl font-bold">Our Mission</h2>
+							<p className="h-1/2 text-xl">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							</p>
+						</div>
+						<div className="w-full h-1/3 flex-grow flex flex-col p-6 bg-white rounded-xl">
+							<h2 className="h-1/4 text-3xl font-bold">ICON HERE</h2>
+							<h2 className="h-1/4 text-3xl font-bold">Our Mission</h2>
+							<p className="h-1/2 text-xl">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+							</p>
+						</div>
+						<div className="w-full h-1/3 flex-grow flex flex-col p-6 bg-white rounded-xl">
+							<h2 className="h-1/4 text-3xl font-bold">ICON HERE</h2>
+							<h2 className="h-1/4 text-3xl font-bold">Our Mission</h2>
+							<p className="h-1/2 text-xl">
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[5vh] pb-[10vh]">
+				<div className="w-[75vw] h-[35vh] rounded-xl bg-red-500">
+
+				</div>
+			</div>
 				
 
     	</main>
