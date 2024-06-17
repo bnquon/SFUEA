9f681e7c59a913c4f6a866806394504073e13163
Brandon Quon <bqa7@sfu.ca>
2024-06-16 22:45:37 -0700

    Working on teams pages

--- app/pages/gamingLounge/page.tsx
+++ app/pages/gamingLounge/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
     	<main className="relative">
 
-      		<div id="section1" className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[15vh] pb-[10vh]">
+      		<div id="section1" className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[20vh] pb-[7.5vh]">
         		<div className="w-[75vw] items-center flex justify-evenly text-black py-10 rounded-xl overflow-hidden relative z-0">
 					<Image className="absolute brightness-50 z-[1]" src='/blob1.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 					<div className="flex flex-col w-3/5 gap-6 text-white z-[2]">
@@ -26,7 +26,7 @@ export default function Home() {
 						<div className="w-1/2 h-[25vh]">
 							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 						</div>
-						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+						<div className="w-1/2 h-[25vh] flex justify-center items-start text-xl">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
@@ -38,13 +38,13 @@ export default function Home() {
 					<p className="text-red-500 text-xl">Icon here</p>
 					<p className="text-5xl mb-[5vh]">Tech Specs</p>
 					<div className="w-full flex gap-6 box-border">
-						<div className="w-1/2 h-[25vh]">
-							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
-						</div>
-						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+						<div className="w-1/2 h-[25vh] flex justify-center items-start text-xl">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+						</div>
+						<div className="w-1/2 h-[25vh]">
+							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 						</div>
 					</div>
 				</div>
@@ -56,7 +56,7 @@ export default function Home() {
 						<div className="w-1/2 h-[25vh]">
 							<Image className="rounded-xl" src='https://images.unsplash.com/photo-1626218174358-7769486c4b79?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
 						</div>
-						<div className="w-1/2 h-[25vh] flex justify-center items-center text-xl">
+						<div className="w-1/2 h-[25vh] flex justify-center items-start text-xl">
 							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 								Ut enim ad minim veniam.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 								labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
--- app/pages/teams/page.tsx
+++ app/pages/teams/page.tsx
@@ -2,18 +2,89 @@ import Image from "next/image";
 
 
 
+
 export default function Home() {
+
+	type Game = {
+        title: string;
+        picture: string;
+    }
+
+    const games: Game[] = [
+        {
+            title: "Valorant",
+            picture: '/val.jpg'
+        },
+        {
+            title: "League of Legends",
+            picture: '/crank.jpg'
+        },
+        {
+            title: "TFT",
+            picture: '/tft.jpg'
+        },
+		{
+			title: 'Overwatch',
+			picture: '/overwatch.jpeg'
+		},
+        {
+            title: 'Fighting Games',
+            picture: '/fighting.jpg'
+        },
+        {
+            title: 'Trading Card Games',
+            picture: '/pokemon.jpg'
+        },
+        {
+            title: 'Destiny 2',
+            picture: '/destiny.jpg'
+        },
+        {
+            title: 'Rainbow Six Siege',
+            picture: '/r6.jpg'
+        },
+        {
+            title: 'FIFA',
+            picture: '/fifa.jpg'
+        },
+        {
+            title: 'Call of Duty',
+            picture: '/cod.jpg'
+        },
+        {
+            title: 'Smite',
+            picture: '/smite.jpg'
+        },
+        {
+            title: 'Pokemon',
+            picture: '/pokemon1.jpg'
+        }
+    ];
+
     return (
-    	<main className="flex flex-col max-w-full overflow-x-hidden">
-      		<div id="section1" className="h-screen w-screen bg-green-900 flex justify-center items-center">
-        		Teams page
+    	<main className="relative">
+
+			<div className="fixed left-[7.5vw] top-[15vh] h-[75vh] w-[2vw] z-10 bg-white"></div>
+
+      		<div id="section1" className="w-screen relative bg-[#d3d3d3] flex flex-col justify-center items-center box-border pt-[15vh] pb-[7.5vh]">
+        		<div className="w-[75vw] items-center flex justify-evenly text-black py-10 rounded-xl overflow-hidden relative z-0">
+					<Image className="absolute brightness-50 z-[1]" src='/blob1.jpg' alt="ex1" width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+					<div className="flex flex-col w-3/5 gap-6 text-white z-[2]">
+						<p className="font-bold text-3xl text-red-500">Gamers' Lounge</p>
+						<p className="text-5xl font-extrabold">SFU GAMING AND ESPORTS CLUB IS THE PREMIUM LIFESTYLE BRAND FOR THE GAMING GENERATION.</p>
+					</div>
+					<Image className="z-[2]" src='/real-logo.png' alt="ex1" width={250} height={250}></Image>
+				</div>
       		</div>
-            <div id="section2" className="h-screen w-screen bg-emerald-900">
-        		
-      		</div>
-      		<div id="section3" className="h-screen w-screen bg-teal-900">
-        		
-      		</div>
+
+			<div className="w-screen bg-[#d3d3d3] flex flex-col justify-center items-center">
+				{games.map((game, index) => (
+					<div key={index} className="w-[75vw] h-[25vh] rounded-xl overflow-hidden relative flex justify-center items-center">
+						<Image src={game.picture} alt={game.title} width={0} height={0} sizes="100vw" style={{ width: '100%', height: '100%', objectFit: 'cover'}}></Image>
+						<p id="game-title" className="absolute text-white text-5xl font-bold z-10">{game.title}</p>
+					</div>
+				))}
+			</div>
     	</main>
   	);
 }
