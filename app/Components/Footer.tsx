f254e8c65570ff1dd678d0a7bfae3d040d2cef9e
Brandon Quon <bqa7@sfu.ca>
2024-07-14 21:48:01 -0700

    Fixed the footer

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
     <div className='pt-[20vh] relative bg-[#d3d3d3]'>
         <div className="w-full lg:h-[50vh] bg-black relative sm:pl-[12.5vw] pl-[5vw]">
 
-            <div className="lg:w-[37.5vw] sm:w-[75vw] w-[90vw] relative bg-red-500 rounded-3xl -translate-y-1/3 flex flex-col justify-between sm:p-10 py-6 px-3 box-border">
+            <div className="text-white lg:w-[37.5vw] sm:w-[75vw] w-[90vw] relative bg-red-500 rounded-3xl -translate-y-1/3 flex flex-col justify-between sm:p-10 py-6 px-3 box-border">
                 <div>
                     <p className='sm:text-5xl text-3xl font-bold'>JOIN THE CLUB.</p> <br />
                     <p className='sm:text-2xl text-xl xl:text-3xl mb-4 xl:mb-8'>We want to see you in the Gamers&apos; Lounge or the next in-house!</p>
@@ -16,7 +16,7 @@ export const Footer = () => {
                 <button id='footer-btn' className='bg-black w-fit rounded-full sm:text-2xl text-lg hover:bg-white hover:text-black duration-200 sm:px-10 sm:py-5 px-6 py-3'>Get Started</button>
             </div>
             
-            <div className='relative sm:w-[75vw] w-[90vw] flex flex-col justify-center items-center lg:justify-start lg:items-start mt-[-20px]'>
+            <div className='relative sm:w-[75vw] w-[90vw] flex flex-col justify-center items-center lg:justify-start lg:items-start mt-[-20px] text-white'>
                 <h1 className='mb-8 text-2xl xl:text-4xl font-bold'>SFU Gaming and Esports Club</h1>
 
                 <ul className='flex gap-8 mb-10 text-xl xl:text-2xl'>
@@ -51,10 +51,10 @@ export const Footer = () => {
 
                 <div className='lg:flex lg:flex-col lg:justify-center lg:items-center lg:gap-4 lg:mb-5 mt-4 sm:mt-4 lg:absolute w-[45%] h-full right-0 top-0 lg:translate-y-[-75%] xl:translate-y-[-50%] flex flex-col items-center justify-center gap-10 xl:gap-14'>
                     <Image src='/real-logo.webp' width={250} height={250} alt='logo'></Image>
-                    <p className='text-xl xl:text-2xl text-center'>Contact us: EMAIL@EMAIL.COM</p>
+                    <p className='text-xl xl:text-2xl text-center text-white'>Contact us: EMAIL@EMAIL.COM</p>
                 </div>
 
-                <div className='flex flex-col justify-center items-center lg:absolute lg:bottom-0 lg:left-1/2 lg:-translate-x-1/2 lg:translate-y-[calc(100%-20px)] mt-4 sm:mt-0'>
+                <div className='flex flex-col justify-center items-center lg:absolute lg:bottom-0 lg:left-1/2 lg:translate-y-[calc(50%+20px)] lg:-translate-x-1/2 sm:mt-0'>
                     <Image src='/racc.webp' width={175} height={175} alt='logo'></Image>
                 </div>
             </div>
